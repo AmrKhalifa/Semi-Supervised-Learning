0fca3f55edd16bc0620d88f9de7ba4924673627a
AmrKhalifa <amr.khalifa.m.a@gmail.com>
2020-04-16 00:06:31 +0200

    tried label propagation

--- data_processor.py
+++ data_processor.py
@@ -6,13 +6,14 @@ import matplotlib.pyplot as plt
 
 train_data_path = '../semi_supervised_data/train_data'
 test_data_path = '../semi_supervised_data/test_data'
+unlabeld_data_path = '../semi_supervised_data/unlabeled'
 def get_set(path): 
 
 	dataset = torchvision.datasets.ImageFolder(
 	    root=path,
 	    transform = transforms.Compose([
-	    transforms.Resize(size = (100, 100)),
-	    transforms.Grayscale(),
+	    transforms.Resize(size = (256, 256)),
+	    #transforms.Grayscale(),
 	    transforms.ToTensor()])
 	)
 
@@ -20,9 +21,11 @@ def get_set(path):
 
 train_set = get_set(train_data_path)
 test_set = get_set(test_data_path)
+unlabeld_set = get_set(unlabeld_data_path)
 
 train_loader = DataLoader(train_set, batch_size = 64, shuffle = True)
 test_loader = DataLoader(test_set, batch_size = 64, shuffle = True)
+unlabeled_loader = DataLoader(unlabeld_set, batch_size = 64, shuffle = True)
 
 if __name__ == "__main__":
 
